3cc4030f91331c63df001200981d0312350740ca
Andrey Makovsky <mak@gettwifi.com>
2018-09-26 10:23:01 +0300

    add output variables

--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,71 @@ output "private ip gitlab" {
 output "public ip gitlab" {
   value = "${digitalocean_droplet.gitlab.*.ipv4_address}"
 }
+
+output "private ip gitlab-runner" {
+  value = "${digitalocean_droplet.gitlab-runner.*.ipv4_address_private}"
+}
+
+output "public ip gitlab-runner" {
+  value = "${digitalocean_droplet.gitlab-runner.*.ipv4_address}"
+}
+
+output "private ip redis" {
+  value = "${digitalocean_droplet.redis.*.ipv4_address_private}"
+}
+
+output "public ip redis" {
+  value = "${digitalocean_droplet.redis.*.ipv4_address}"
+}
+
+output "private ip cassandra" {
+  value = "${digitalocean_droplet.cassandra.*.ipv4_address_private}"
+}
+
+output "public ip cassandra" {
+  value = "${digitalocean_droplet.cassandra.*.ipv4_address}"
+}
+
+output "private ip postgresql-master" {
+  value = "${digitalocean_droplet.postgresql-master.*.ipv4_address_private}"
+}
+
+output "public ip postgresql-master" {
+  value = "${digitalocean_droplet.postgresql-master.*.ipv4_address}"
+}
+
+output "private ip postgresql-slave" {
+  value = "${digitalocean_droplet.postgresql-slave.*.ipv4_address_private}"
+}
+
+output "public ip radius" {
+  value = "${digitalocean_droplet.radius.*.ipv4_address}"
+}
+
+output "private ip radius" {
+  value = "${digitalocean_droplet.radius.*.ipv4_address_private}"
+}
+
+output "public ip softether" {
+  value = "${digitalocean_droplet.softether.*.ipv4_address}"
+}
+
+output "private ip softether" {
+  value = "${digitalocean_droplet.softether.*.ipv4_address_private}"
+}
+
+output "public ip asterisk" {
+  value = "${digitalocean_droplet.asterisk.*.ipv4_address}"
+}
+
+output "private ip asterisk" {
+  value = "${digitalocean_droplet.asterisk.*.ipv4_address_private}"
+}
+
+output "public ip web" {
+  value = "${digitalocean_droplet.web.*.ipv4_address}"
+}
+
+output "private ip web" {
+  value = "${digitalocean_droplet.web.*.ipv4_address_private}"
+}
